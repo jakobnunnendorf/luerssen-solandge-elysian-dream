e0ea1ec6a165e4a7fbc3a82b76771d4b16511c3f
Jakob <jnunnendorf@icloud.com>
2024-07-26 15:30:14 +0200

    feat(app/page.tsx): add bg image

---
+++ src/app/hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Image from "next/image";
+
+function Hero() {
+    return (
+        <figure className="relative h-fit w-full ">
+            <Image
+                src="/luerssen-bg.jpg"
+                alt="A stunning luxury motor yacht named 'Elysian Dream' inspired by the Lürssen Solandge. The yacht is cruising on clear blue waters, showcasing its sleek and modern design."
+                width={1440} height={634}
+                className="z-0"
+            />
+            <div className="absolute top-0 w-full h-full bg-black opacity-70 z-10"></div>
+        </figure>
+    );
+}
+
+export default Hero;
