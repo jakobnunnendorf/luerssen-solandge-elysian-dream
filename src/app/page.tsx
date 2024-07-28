4d6bed224defba74a8db2acdbcc121927b80031d
Jakob <jnunnendorf@icloud.com>
2024-07-28 14:13:09 +0200

    feat(page): integrate components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import Hero from "./Hero";
 import React from "react";
-import Introduction from "@/app/Introduction/Introduction";
+import Introduction from "./Introduction/Introduction";
+import Background from "./Background/Background";
+import InfoCards from "./InfoCards/InfoCards";
+import Milestones from "./Milestones/Milestones";
+import GalleryPreview from "@/app/GalleryPreview";
 
 export default function Home() {
   return (
       <main className="min-h-screen relative ">
           <Hero/>
           <Introduction/>
+          <Background/>
+          <InfoCards/>
+          <Milestones/>
+          <GalleryPreview/>
       </main>
   );
 }
