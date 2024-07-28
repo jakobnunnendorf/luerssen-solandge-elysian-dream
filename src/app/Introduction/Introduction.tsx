70edee9592e5b27703ca955334fd96e5f2bfa0b0
Jakob Nunnendorf <jakob.nunnendorf@ebf.com>
2024-07-28 20:21:09 +0200

    style(Introduction): finish Introduction.tsx desktop

--- src/app/Introduction/Introduction.tsx
+++ src/app/Introduction/Introduction.tsx
@@ -1,38 +1,65 @@
 import React from 'react';
-import Image from 'next/image'
+import Image from 'next/image';
 import Link from "next/link";
 
-function Introduction() {
+function Feature({ feature }: { feature: string }) {
     return (
-        <div className=" flex px-24 gap-x-8 h-fit">
-            <div className=" relative">
-                <Image src="/designer.png" alt="" width={502} height={680}/>
-            </div>
-            <div className="col-span-2 w-full h-4/5">
-                <div>
-                    <h2 className="text-4xl">Brief Introduction to the Luxury Yacht</h2>
-                    <p className="">The &quot;Elysian Dream&quot; embodies the pinnacle of opulence and elegance on the water.
+        <li className="flex justify-left  gap-x-2 items-center text-lg">
+            <figure className="h-4 w-4">
+                <Image src="/bullet.png" width={15} height={15} alt="check mark" />
+            </figure>
+            <p>{feature}</p>
+        </li>
+    );
+}
+
+function Introduction() {
+    const features = [
+        "Sleek Design",
+        "Multiple Decks",
+        "Luxurious Interiors",
+        "Spacious Accommodations",
+        "Advanced Technology",
+        "Entertainment and Leisure",
+        "Gourmet Dining",
+        "Water Toys and Tenders",
+        "Professional Crew"
+    ];
+
+    return (
+        <div className="flex h-screen gap-8 p-24 ">
+            <figure className="relative h-full flex-shrink-0 ">
+                <Image
+                    src="/designer.png" alt="Designer"
+                    width={502} height={680}
+                    className="h-full w-full" />
+            </figure>
+            <div className="flex flex-col col-span-2 w-full py-2 gap-8 h-full">
+                <div className="flex flex-col gap-5">
+                    <h2 className="text-5xl font-serif">
+                        Brief Introduction to the <span className="text-[#A37C45]">Luxury Yacht</span>
+                    </h2>
+                    <p>
+                        The &quot;Elysian Dream&quot; embodies the pinnacle of opulence and elegance on the water.
                         This exquisite yacht offers unparalleled comfort and sophistication,
                         ensuring an unforgettable yachting experience. With state-of-the-art amenities, bespoke
-                        interiors,
-                        and a timeless design, the &quot;Elysian Dream&quot; is your sanctuary on the high seas.</p>
+                        interiors, and a timeless design, the &quot;Elysian Dream&quot; is your sanctuary on the high seas.
+                    </p>
                 </div>
                 <div>
-                    <h2>Key Features and Highlights</h2>
-                    <ul>
-                        <li>Sleek Design</li>
-                        <li>Multiple Decks</li>
-                        <li>Luxurious Interiors</li>
-                        <li>Spacious Accommodations</li>
-                        <li>Advanced Technology</li>
-                        <li>Entertainment and Leisure</li>
-                        <li>Gourmet Dining</li>
-                        <li>Water Toys and Tenders</li>
-                        <li>Professional Crew</li>
+                    <h2 className="text-3xl font-serif py-2 pb-5">Key Features and Highlights</h2>
+                    <ul className="grid grid-cols-2 gap-y-5 gap-x-28 w-full">
+                        {features.map((feature, index) => (
+                            <Feature key={index} feature={feature} />
+                        ))}
                     </ul>
                 </div>
-                <div>
-                    <Link href="/">Read more</Link>
+                <div className="pt-4">
+                    <Link href="/" >
+                        <div className="flex justify-center items-center bg-[#A37C45] w-44 h-14 px-4 py-2  rounded-sm ">
+                            <p className="font-serif text-white text-center text-lg">Read more</p>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
