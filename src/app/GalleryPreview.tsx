6a19877a2e885bf0371d1cee99f4d68be584d38f
Jakob <jnunnendorf@icloud.com>
2024-08-11 14:22:29 +0800

    style(GalleryPreview.tsx)

--- src/app/GalleryPreview.tsx
+++ src/app/GalleryPreview.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 function GalleryPreview() {
     return (
-        <div className="grid place-content-center">
-            <h2>Gallery</h2>
+        <div className="grid place-content-center pt-20">
+            <h2 className="text-4xl text-center pb-12">Gallery</h2>
             <Image src="/gallery.png" alt="Test" height={1240} width={1065} />
         </div>
     );
